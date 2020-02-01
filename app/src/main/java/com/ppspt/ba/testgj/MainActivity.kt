254969b8f12c216ee18fca6169d419f0fc3ed230
Giuseppe Fabio Trentadue <gf.trentadue@gmail.com>
2020-02-01 14:30:35 +0100

    Created project. Add dependecy for retrofit.

---
+++ app/src/main/java/com/ppspt/ba/testgj/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ppspt.ba.testgj
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
